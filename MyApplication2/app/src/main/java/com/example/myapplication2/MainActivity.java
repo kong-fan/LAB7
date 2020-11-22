dbae137e7c76afdeb718de774657db084f8fbd50
kong-fan <tom23393306@gmail.com>
2020-11-22 11:19:08 +0800

    second

--- MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -44,7 +46,57 @@ public class MainActivity extends AppCompatActivity {
                     runAsyncTask();
             }
             private void runAsyncTask(){
-                
+                new AsyncTask<Void,Integer,Boolean>(){
+                    protected void onPreExecute(){
+                        super.onPreExecute();
+                        tv_weight.setText("標準體重\n無");
+                        tv_bmi.setText("體脂肪\n無");
+                        progressBar2.setProgress(0);
+                        tv_progress.setText("0%");
+                        ll_progress.setVisibility(View.VISIBLE);
+                    }
+
+                    protected Boolean doInBackground(Void... voids){
+                        int progress=0;
+
+                        while(progress <= 100){
+                            try {
+                                Thread.sleep(50);
+                                publishProgress(progress);
+                                progress++;
+                            }catch (InterruptedException e){
+                                e.printStackTrace();
+                            }
+                        }
+                        return true;
+                    }
+                    protected void onProgressUpdate(Integer... values){
+                        super.onProgressUpdate(values);
+                        progressBar2.setProgress(values[0]);
+                        tv_progress.setText(values[0]+"%");
+                    }
+                    protected void onPostExecute(Boolean aBoolean){
+                        super.onPostExecute(aBoolean);
+                        ll_progress.setVisibility(View.GONE);
+
+                        int h=Integer.valueOf(
+                                ed_height.getText().toString());
+                        int w=Integer.valueOf(
+                                ed_weight.getText().toString());
+                        double standWeight,bodyFat;
+                        if(btn_boy.isChecked()){
+                            standWeight=(h-80)*0.7;
+                            bodyFat=(w-0.88*standWeight)/w*100;
+                        }else {
+                            standWeight=(h-70)*0.6;
+                            bodyFat=(w-0.82*standWeight)/w*100;
+                        }
+                        tv_weight.setText(String.format("標準體重\n%.2f",standWeight));
+
+                        tv_bmi.setText(String.format("體脂肪\n%.2f",bodyFat));
+
+                    }
+                }.execute();
             }
         });
 
